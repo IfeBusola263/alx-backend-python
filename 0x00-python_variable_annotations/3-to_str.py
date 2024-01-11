388e4fe9560fb29dc0b44f11e15a2dfab29898ce
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-11 15:26:14 +0000

    Implemented string conversion

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+This module holds an annotated function for stringifying a float.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    The function returns a str representation of the float n.
+    '''
+    return str(n)
