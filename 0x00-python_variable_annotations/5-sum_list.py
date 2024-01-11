2c0daf61f42a197bd72fcf77146f3ceed54ba98d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-11 15:36:19 +0000

    Implemented the sum function for a list with annotations

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -4,6 +4,7 @@ The module hold a function that returns a value
 '''
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     '''
     The function takes a list of floats and returns the sum.
