9a2e96a619b48c975775c5dced4d11a500d94a2e
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-11 15:04:13 +0000

    Annotated a sum function

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+This module houses a function for type annotation.
+'''
+
+def add(a: float, b: float) -> float:
+    '''This function adds the argument.
+    Returns a float
+    '''
+    return a + b
