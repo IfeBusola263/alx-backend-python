ac15ca2a536586f1d427c9ff17717bd2e6b23ec1
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-11 15:23:05 +0000

    Annotated function to return floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+The module hold an annotated function to get the floor division
+of a float.
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    The function returns the floor(whole number after the decimal) of the
+    float n
+    '''
+    return int(n)
