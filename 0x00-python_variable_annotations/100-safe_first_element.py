67fa751f90c4b7fa0022792c5dcc4d46adadfa17
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-11 16:41:48 +0000

    Annotated the function

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+'''
+This module holds function that checks for the first element in an
+input, but it has to be safe annotated first before operations can
+pull through.
+'''
+from typing import Sequence, Any, Union
+
+
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''
+    This function returns the first element of a sequence
+    '''
+    if lst:
+        return lst[0]
+    else:
+        return None
