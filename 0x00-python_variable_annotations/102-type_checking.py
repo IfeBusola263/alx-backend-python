4ca58ca3b712ca0a24b4137f9fd6d573e249a5e2
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-11 17:35:49 +0000

    Debugged the code and properly type annotated it

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+This module houses a function that returns a list of items.
+'''
+from typing import List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''
+    This function returns a list of items in the given Tuple.
+    based on the number of factors given, being 2 minimum.
+    '''
+    zoomed_in: Tuple = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3.0)
