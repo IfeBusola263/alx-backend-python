938d91da631c1b6269606f7120bf250959c0d863
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-11 15:18:02 +0000

    An annotated function to concatenate strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+This module has the concatenate function which is annotated
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    The function concatenates two strings and returns the
+    result.
+    '''
+    return str1 + str2
