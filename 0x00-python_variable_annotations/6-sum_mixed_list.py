cd6901f5b5346adab33f84e09e20fbaa2117d013
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-11 15:45:47 +0000

    Changed the short hand

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ The module hold a function that returns a value
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     '''
     The function takes a list of floats and ints and returns the sum.
     '''
