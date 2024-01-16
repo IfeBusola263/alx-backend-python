eea66d9b112f63a270b6d2130ee5ae0f88adb385
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-16 07:52:40 +0000

    Implemented a generator function

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+A module for coroutines
+'''
+import random
+import asyncio
+
+
+async def async_generator() -> int:
+    '''
+    The generator function returns a random number between
+    0 and 10.
+    '''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
