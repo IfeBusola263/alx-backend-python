0101f3bdf19459937bf8105cb93cfcf001b41758
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-16 11:21:42 +0000

    runtime measure

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+A module for coroutines
+'''
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''
+    The coroutine returns time for all the session to run.
+    '''
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(),
+                               async_comprehension(),
+                               async_comprehension(),
+                               )
+    elapsed = time.perf_counter() - start
+    return elapsed
