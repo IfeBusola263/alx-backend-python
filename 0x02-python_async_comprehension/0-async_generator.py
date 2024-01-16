3bb2c01820b71f3c08337690119b46b5c940784d
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-01-16 11:33:05 +0000

    changed the type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ A module for coroutines
 '''
 import random
 import asyncio
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     '''
     The generator function returns a random number between
     0 and 10.
