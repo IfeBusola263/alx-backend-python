b9a36e5739f853a6314902f852ab8da193cef680
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-02 22:28:40 +0000

    tested the get json method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,9 @@ This is a test Module for the utils module
 functions.
 """
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -35,3 +36,26 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError, msg=expected) as err:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    This is the class test for all the expected outputs of the
+    get_json method in the utils module.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+        ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mocked_req):
+        """
+        Test for the get_json function.
+        """
+        mock_response = Mock()
+        mock_response.json = lambda: test_payload
+        mocked_req.return_value = mock_response
+        self.assertEqual(get_json(test_url), test_payload)
+        self.assertEqual(mock_response.json(), test_payload)
+        mocked_req.assert_called_once_with(test_url)
