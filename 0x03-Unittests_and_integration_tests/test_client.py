486a8a425b07dc439249542a95a2c9ae58a7c445
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-05 12:24:17 +0000

    Worked on the formating

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -5,12 +5,13 @@ This module is a unit test for the client module.
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from client import GithubOrgClient
+from client import GithubOrgClient, get_json
 
 
 class TestGithubClient(unittest.TestCase):
     '''
     This is a test for the githuborg client method.
+    To check if it has the correct return value.
     '''
     @parameterized.expand([
         ('google',),
@@ -26,4 +27,4 @@ class TestGithubClient(unittest.TestCase):
         test = GithubOrgClient(org)
         self.assertEqual(test.org, {'name': org})
         mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org}")
+            "https://api.github.com/orgs/{}".format(org))
