609247350f250907262c0ce5e9f95868410784fe
Busola Ogunbayo <ifebusolaj95@gmail.com>
2024-02-05 10:50:04 +0000

    Implemented mock to tested the memorized function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,9 +4,9 @@ This is a test Module for the utils module
 functions.
 """
 import unittest
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch, Mock, PropertyMock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -59,3 +59,40 @@ class TestGetJson(unittest.TestCase):
         self.assertEqual(get_json(test_url), test_payload)
         self.assertEqual(mock_response.json(), test_payload)
         mocked_req.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    This is the test for the memoize decorator in the utils
+    module.
+    """
+
+    def test_memoize(self):
+        """
+        This method test the decorator and all integration necessities expected
+        of the decorator.
+        """
+        class TestClass:
+            """
+            Test Class for memoize
+            """
+
+            def a_method(self):
+                """
+                A method that returns 42
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """
+                A method that invokes the a_method.
+                """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_aMeth:
+            test = TestClass()
+            mock_aMeth.return_value = 150
+            self.assertEqual(test.a_property, 150)
+            self.assertEqual(test.a_property, 150)
+            mock_aMeth.assert_called_once()
